680a406aa5382e0b5dcc32fecb779f57ab18d239
Aarman K Kadri <aarman1922030@gmail.com>
2025-06-22 20:56:57 +0530

    Median array problem solved

---
+++ median_array.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        double ans;
+        for(int i=0; i<nums2.size(); i++) {
+            nums1.emplace_back(nums2[i]);
+        }
+    
+        sort(nums1.begin(), nums1.end());
+        int n = nums1.size();
+        if(n % 2 == 0) {
+            ans = (nums1[n/2-1] + nums1[n/2]) / 2.0;
+        }
+        else {
+            ans = nums1[n/2-1];
+        }
+        // cout << ans;
+        return ans;
+    }
+
+};
+
+int main() {
+    vector<int> num1 = {1,2,3,4,7};
+    vector<int> num2 = {5,6,8,9,10};
+    Solution obj;
+    double ans = obj.findMedianSortedArrays(num1, num2);
+    cout << ans;
+    return 0;
+}
